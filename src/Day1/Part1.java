175ae2c42a3e9afbee1287b18372a325f82b5905
Jesper Lindberg <JesperLindberg92@protonmail.com>
2024-12-01 20:56:47 +0100

    Day 1 Completed

---
+++ src/Day1/Part1.java
@@ -0,0 +1,46 @@
+package Day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Part1 {
+
+    List<Integer> leftList = new ArrayList<>();
+    List<Integer> rightList = new ArrayList<>();
+    int totalDistance = 0;
+
+    Part1(){
+        parseData();
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+        for (int i = 0; i < leftList.size(); i++){
+            totalDistance += getDistance(leftList.get(i), rightList.get(i));
+        }
+        System.out.println(totalDistance);
+    }
+
+    public int getDistance(int left, int right){
+        return Math.abs(left - right);
+    }
+
+    public void parseData(){
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Day1/data"))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] lines = line.split("\\s+");
+                leftList.add(Integer.parseInt(lines[0]));
+                rightList.add(Integer.parseInt(lines[1]));
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Part1();
+    }
+}
---
+++ src/Day1/Part2.java
@@ -0,0 +1,58 @@
+package Day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Part2 {
+
+    List<Integer> leftList = new ArrayList<>();
+    List<Integer> rightList = new ArrayList<>();
+    int similarityScore = 0;
+
+    Part2(){
+        parseData();
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+        int timesOverlapped;
+        for (int i = 0; i < leftList.size(); i++){
+            timesOverlapped = getAmountOfTimesValuesOverlap(leftList.get(i));
+            similarityScore += getSimilarityScore(leftList.get(i), timesOverlapped);
+        }
+        System.out.println(similarityScore);
+    }
+
+    public int getAmountOfTimesValuesOverlap(int data){
+        int total = 0;
+        for (int i = 0; i < rightList.size(); i++) {
+            if (rightList.get(i) == data){
+                total++;
+            }
+        }
+        return total;
+    }
+
+    public int getSimilarityScore(int value, int multiplier){
+        return value * multiplier;
+    }
+
+    public void parseData(){
+        try (BufferedReader br = new BufferedReader(new FileReader("src/Day1/data"))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                String[] lines = line.split("\\s+");
+                leftList.add(Integer.parseInt(lines[0]));
+                rightList.add(Integer.parseInt(lines[1]));
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Part2();
+    }
+}
